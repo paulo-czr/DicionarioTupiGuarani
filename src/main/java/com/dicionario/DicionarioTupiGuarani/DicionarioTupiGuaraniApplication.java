8c13a41cbc0140f30da8afc63de21a174d7e0127
Paulo Cesar <PauloCesarCoder@gmail.com>
2026-04-03 10:16:12 -0300

    Dicionario-TupiGuarani

---
+++ src/main/java/com/dicionario/DicionarioTupiGuarani/DicionarioTupiGuaraniApplication.java
@@ -0,0 +1,13 @@
+package com.dicionario.DicionarioTupiGuarani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicionarioTupiGuaraniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicionarioTupiGuaraniApplication.class, args);
+	}
+
+}
