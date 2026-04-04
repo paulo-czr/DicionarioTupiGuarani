b0e08842232b30a447c0d07bbbc464bcd336e3d4
Paulo Cesar <PauloCesarCoder@gmail.com>
2026-04-04 16:52:21 -0300

    adicionando .env e configurações para variáveis de ambiente

--- src/main/java/com/dicionario/DicionarioTupiGuarani/DicionarioTupiGuaraniApplication.java
+++ src/main/java/com/dicionario/DicionarioTupiGuarani/DicionarioTupiGuaraniApplication.java
@@ -3,10 +3,21 @@ package com.dicionario.DicionarioTupiGuarani;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class DicionarioTupiGuaraniApplication {
 
 	public static void main(String[] args) {
+
+		// Carrega o arquivo .env da raiz do projeto
+        Dotenv dotenv = Dotenv.configure()
+            .ignoreIfMissing()
+            .load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
 		SpringApplication.run(DicionarioTupiGuaraniApplication.class, args);
 	}
 
